0807b6722885af0bb01ed867ee9dc7ddcd3848a3
Nikhil <nikhilshah.net@hotmail.com>
2023-02-17 10:55:58 +0530

    DI Demo

---
+++ Day 7/DI_demo/DI_demo/Controllers/EmployeeController.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DI_demo.Models;
+namespace DI_demo.Controllers
+{
+    public class EmployeeController : Controller
+    {
+
+        //This is a very bad practice
+        // Employee empObj = new Employee(); 
+        // we do not have any code to destroy this object
+
+
+        Employee _empObj;
+
+        public EmployeeController(Employee empObjREF) //reference to the object created by runtime
+        {
+            _empObj = empObjREF;
+        }
+
+
+
+        public IActionResult ShowAllEmployees()
+        {
+            ViewBag.allEmp = _empObj.GetAllEmployees();
+            return View();
+        }
+
+        public IActionResult SearchEmployee()
+        {
+            ViewBag.message = "";
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult SearchEmployee(int empNo)
+        {
+            try
+            {
+                ViewBag.message = "";
+                ViewBag.emp = _empObj.GetEmpById(empNo);
+                return View();
+            }
+            catch (Exception es)
+            {
+                ViewBag.message = es.Message;
+                return View();
+            }
+           
+        }
+
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day 7/DI_demo/DI_demo/Models/Employee.cs
@@ -0,0 +1,36 @@
+﻿namespace DI_demo.Models
+{
+    public class Employee
+    {
+        public int eId { get; set; }
+        public string eName { get; set; }
+        public string eDesigtnation { get; set; }
+        public double eSalary { get; set; }
+        public bool eIsPermenant { get; set; }
+
+        static List<Employee> eLIst = new List<Employee>()
+        {
+            new Employee(){ eId=101, eName="Karan", eDesigtnation="Sales", eIsPermenant=true, eSalary=4000},
+            new Employee(){ eId=102, eName="Rohan", eDesigtnation="HR", eIsPermenant=true, eSalary=14000},
+            new Employee(){ eId=103, eName="Mohan", eDesigtnation="Sales", eIsPermenant=true, eSalary=24000},
+            new Employee(){ eId=104, eName="Sohan", eDesigtnation="Sales", eIsPermenant=false, eSalary=34000},
+            new Employee(){ eId=105, eName="Priya", eDesigtnation="HR", eIsPermenant=true, eSalary=44000},
+            new Employee(){ eId=106, eName="Riya", eDesigtnation="Sales", eIsPermenant=false, eSalary=7000},
+        };
+
+        public List<Employee> GetAllEmployees()
+        {
+            return eLIst;
+        }
+
+        public Employee GetEmpById(int id)
+        {
+            var emp = eLIst.Find(e => e.eId == id);
+            if (emp !=null)
+            {
+                return emp;
+            }
+            throw new Exception("Employee Not Found");
+        }
+    }
+}
