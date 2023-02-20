76b1bb03cdfe093f1e3d54813fda5f96c93cf15e
Nikhil <nikhilshah.net@hotmail.com>
2023-02-20 10:53:48 +0530

    Identity framework

---
+++ Day 8/bankingAPP_IdentityFramework/bankingAPP_IdentityFramework/Controllers/BankingController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bankingAPP_IdentityFramework.Controllers
+{
+    [Authorize]
+    public class BankingController : Controller
+    {
+
+        [AllowAnonymous]
+        public IActionResult BankingHome()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult TypeOfAccounts()
+        {
+            return View();
+        }
+        public IActionResult DownloadStatement()
+        {
+            return View();
+        }
+        public IActionResult TransferFunds()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Day 8/bankingAPP_IdentityFramework/bankingAPP_IdentityFramework/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace bankingAPP_IdentityFramework.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
